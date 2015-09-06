fa5debbcc1c90011f093f7fbc610a132bc7e8d09
artem.stepanenko <artem.stepanenko@rocket-internet.de>
2015-09-06 14:05:11 +0200

    A first ViewModel unit test is done

--- PetrMazepaTests/ArticlesViewModelTests.swift
+++ PetrMazepaTests/ArticlesViewModelTests.swift
@@ -10,26 +10,35 @@ import XCTest
 
 class ArticlesViewModelTests: XCTestCase {
     
+    var model = ArticlesViewModel()
+    
     override func setUp() {
+        
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.model = ArticlesViewModel()
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func testThatLoadsArticlesIfThereAreNoArticles() {
+        
+        let expectation = self.expectationWithDescription("Loading state changed")
+        
+        self.model.loadingStateChanged = { (loading: Bool) in
+
+            XCTAssert(loading)
+            expectation.fulfill()
         }
+        
+        self.model.articlesInserted = { (range: Range<Int>) in
+            // do nothing
+        }
+        
+        self.model.viewDidLoad()
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: { (error: NSError?) in
+            if error != nil {
+                XCTFail("Doesn't load articles")
+            }
+        })
     }
     
 }
