355888f6c39fe492c01e28780437ff4be042357d
artem.stepanenko <artem.stepanenko@rocket-internet.de>
2015-10-17 18:29:09 +0200

    The search screen appearance is fixed

--- PetrMazepa/Source/SearchViewController.swift
+++ PetrMazepa/Source/SearchViewController.swift
@@ -40,14 +40,11 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
         
         super.viewDidAppear(animated)
         self.startHandlingKeyboardAppearance()
-        self.view.layoutIfNeeded()
-        self.searchBar.becomeFirstResponder()
     }
     
     override func viewWillDisappear(animated: Bool) {
         
         super.viewWillDisappear(animated)
-        self.searchBar.resignFirstResponder()
         self.stopHandlingKeyboardAppearance()
     }
     
