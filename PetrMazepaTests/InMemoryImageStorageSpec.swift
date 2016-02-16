a827c5455315907baead4fc01bdedc716f620903
Artem Stepanenko <artem.stepanenko@rocket-internet.de>
2016-02-16 15:49:08 +0100

    InMemoryImageStorage unit testing is in progress

--- PetrMazepaTests/InMemoryImageStorageSpec.swift
+++ PetrMazepaTests/InMemoryImageStorageSpec.swift
@@ -11,21 +11,44 @@ import Nimble
 
 class InMemoryImageStorageSpec: QuickSpec {
     override func spec() {
+        
+        var storage: InMemoryImageStorage!
+        
+        beforeEach {
+            storage = InMemoryImageStorage()
+        }
+        
+        describe("the storage loading works properly") {
+            context("when an image is specified only with URL") {
+                
+                var image: UIImage!
+                var imageSpec: ImageSpec!
+                
+                beforeEach {
+                    
+                    storage = InMemoryImageStorage()
+                    image = self.imageNamed("chersonesus")
+                    imageSpec = ImageSpec(url: NSURL(string: "http://test.petrimazepa.com/bundles/pim/images/thumbs/1.jpeg")!)
+                    storage.saveImage(spec: imageSpec, image: image)
+                }
+                
+                it("returns an image if exists") {
 
-        describe("an in-memory image storage") {
-            
-            var storage: InMemoryImageStorage!
-            beforeEach {
-                storage = InMemoryImageStorage()
-            }
-            
-            describe("test") {
-                context("a test context") {
-                    it("works perfectly") {
-                        expect(1 + 1).to(equal(3))
-                    }
+                    let returnedImage = storage.loadImage(spec: imageSpec)
+                    expect(returnedImage) != nil
+                }
+                
+                it("returns nil if doesn't exist") {
+
+                    let anotherImageSpec = ImageSpec(url: NSURL(string: "http://test.petrimazepa.com/bundles/pim/images/thumbs/2.jpeg")!)
+                    let returnedImage = storage.loadImage(spec: anotherImageSpec)
+                    expect(returnedImage) == nil
                 }
             }
         }
+    }
+    
+    private func imageNamed(name: String) -> UIImage! {
+        return UIImage(named: "chersonesus", inBundle: NSBundle(forClass: self.classForCoder), compatibleWithTraitCollection: nil)!
     }
 }
--- Podfile
+++ Podfile
@@ -1,8 +1,9 @@
+platform :ios, '8.0'
 use_frameworks!
 
 def testing_pods
-    pod 'Quick', '~> 0.9.1'
-    pod 'Nimble', '3.1.0'
+    pod 'Quick'
+    pod 'Nimble'
 end
 
 target 'PetrMazepaTests', :exclusive => true do
