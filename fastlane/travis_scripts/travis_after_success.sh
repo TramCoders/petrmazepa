1cc4d694d5526f4c16b840d699ecf51674b7d6db
Roman Kopaliani <roman.kopaliani.dev@gmail.com>
2016-05-27 16:43:15 +0300

    WIP

--- fastlane/travis_scripts/travis_after_success.sh
+++ fastlane/travis_scripts/travis_after_success.sh
@@ -2,9 +2,7 @@
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
 	fastlane ios report_pr_test_passed
 	exit $?
-fi
-
-if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
+elif [[ "$TRAVIS_BRANCH" == "develop" ]]; then
 	fastlane ios report_test_coverage
 	exit $?
 fi
