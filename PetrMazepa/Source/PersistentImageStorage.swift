708db92b48781f14d505208228900a04e62b768f
Roman Kopaliani <roman.kopaliani.dev@gmail.com>
2016-05-31 14:53:44 +0300

    Updated intends

--- PetrMazepa/Source/PersistentImageStorage.swift
+++ PetrMazepa/Source/PersistentImageStorage.swift
@@ -10,26 +10,26 @@ import Foundation
 
 class PersistentImageStorage {
 
-	private let cacheFolderPath: String = {
+    private let cacheFolderPath: String = {
 
-		let paths = NSSearchPathForDirectoriesInDomains(.CachesDirectory, .UserDomainMask, true) as [String]
-		let path = paths[0] as NSString
-		return path.stringByAppendingPathComponent("\(NSBundle.mainBundle().bundleIdentifier!).images")
-	}()
+        let paths = NSSearchPathForDirectoriesInDomains(.CachesDirectory, .UserDomainMask, true) as [String]
+        let path = paths[0] as NSString
+        return path.stringByAppendingPathComponent("\(NSBundle.mainBundle().bundleIdentifier!).images")
+    }()
 
-	private let fileManager: NSFileManager
+    private let fileManager: NSFileManager
 
-	init() {
-	
-		fileManager = NSFileManager.defaultManager()
-		if !fileManager.fileExistsAtPath(cacheFolderPath) {
-			do {
-				try fileManager.createDirectoryAtPath(cacheFolderPath, withIntermediateDirectories: true, attributes: nil)
-			} catch {
-				// TODO: handle
-			}
-		}
-	}
+    init() {
+
+        fileManager = NSFileManager.defaultManager()
+        if !fileManager.fileExistsAtPath(cacheFolderPath) {
+            do {
+                try fileManager.createDirectoryAtPath(cacheFolderPath, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                // TODO: handle
+            }
+        }
+    }
 }
 
 //MARK: ImageStorage
@@ -39,69 +39,69 @@ extension PersistentImageStorage: ImageStorage {
 
     func clear() {      // not the best implementation
 
-		guard let imageNames = try? fileManager.contentsOfDirectoryAtPath(cacheFolderPath) else {
-			return
-		}
-		imageNames.forEach {
-			imageName in
-			do {
-				try fileManager.removeItemAtPath("\(cacheFolderPath)/\(imageName)")
-			} catch {
-				assertionFailure("Unable to remove from cache folder file with name \(imageName)")
-			}
-		}
+        guard let imageNames = try? fileManager.contentsOfDirectoryAtPath(cacheFolderPath) else {
+            return
+        }
+        imageNames.forEach {
+            imageName in
+            do {
+                try fileManager.removeItemAtPath("\(cacheFolderPath)/\(imageName)")
+            } catch {
+                assertionFailure("Unable to remove from cache folder file with name \(imageName)")
+            }
+        }
     }
-    
+
     func saveImage(withSpec spec: ImageSpec, image data: NSData) {
         data.writeToFile(self.filePath(forSpec: spec), atomically: false)
     }
-    
+
     func loadImage(withSpec spec: ImageSpec) -> NSData? {
-        
+
         let filePath = self.filePath(forSpec: spec)
-		return NSData(contentsOfFile: filePath) ?? nil
+        return NSData(contentsOfFile: filePath) ?? nil
     }
 }
 
 //MARK: File Attribute – Path
 private extension PersistentImageStorage {
 
-	func filePath(forSpec spec: ImageSpec) -> String {
+    func filePath(forSpec spec: ImageSpec) -> String {
 
-		let key = self.key(forSpec: spec)
-		let fileName = key.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
-		return cacheFolderPath.stringByAppendingString("/\(fileName)")
-	}
+        let key = self.key(forSpec: spec)
+        let fileName = key.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
+        return cacheFolderPath.stringByAppendingString("/\(fileName)")
+    }
 
-	func fileNames() -> [String]? {
+    func fileNames() -> [String]? {
 
-		guard let paths = try? fileManager.subpathsOfDirectoryAtPath(cacheFolderPath) else {
-			return nil
-		}
-		return paths
-	}
+        guard let paths = try? fileManager.subpathsOfDirectoryAtPath(cacheFolderPath) else {
+            return nil
+        }
+        return paths
+    }
 }
 
 //MARK: File Attribute – Size
 extension PersistentImageStorage {
 
-	func sizeInBytes() -> UInt64 {
+    func sizeInBytes() -> UInt64 {
 
-		guard let fileNames = fileNames() else {
-			return 0
-		}
+        guard let fileNames = fileNames() else {
+            return 0
+        }
 
-		return fileNames.reduce(0, combine: { (value, fileName) -> UInt64 in
-			return value + self.sizeOfFileInBytes(fileName)
-		})
-	}
-
-	private func sizeOfFileInBytes(fileName: String) -> UInt64 {
-
-		let filePath = cacheFolderPath.stringByAppendingFormat("/%@", fileName)
-		guard let attrs = try? fileManager.attributesOfItemAtPath(filePath) as NSDictionary else {
-			return 0
-		}
-		return attrs.fileSize()
-	}
+        return fileNames.reduce(0, combine: { (value, fileName) -> UInt64 in
+            return value + self.sizeOfFileInBytes(fileName)
+        })
+    }
+    
+    private func sizeOfFileInBytes(fileName: String) -> UInt64 {
+        
+        let filePath = cacheFolderPath.stringByAppendingFormat("/%@", fileName)
+        guard let attrs = try? fileManager.attributesOfItemAtPath(filePath) as NSDictionary else {
+            return 0
+        }
+        return attrs.fileSize()
+    }
 }
